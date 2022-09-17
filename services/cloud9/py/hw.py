050c8d220d3b87e57b489aa71fcbb235e74f0a5a
Paul Ramnora <pramnora@yahoo.com>
2022-09-17 18:50:36 +0100

    Create hw.py
    Initial commit: Hello, world! - Python 3/Included code comments.

---
+++ services/cloud9/py/hw.py
@@ -0,0 +1,12 @@
+'''
+ I created/and, ran this Python 3 script using Amazon Cloud 9 platform;
+ and, it ran successfully. 
+
+ NOTE: One just needs to select menu option: 
+       Run With...Python 3.
+  
+ Created: Sat 17 Sep 2022 18:47 PM GMT
+ Updated: Sat 17 Sep 2022 18:47 PM GMT    
+'''
+
+print("Hi, from Python!")

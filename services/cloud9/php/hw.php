7963102026feef7b9f9b648edc9332db9ab03769
Paul Ramnora <pramnora@yahoo.com>
2022-09-17 19:19:37 +0100

    Update hw.php
    Minor edits: in -> into/added line: (each of which I saw when running the server)

--- services/cloud9/php/hw.php
+++ services/cloud9/php/hw.php
@@ -8,14 +8,14 @@
 But, the cloud9 editor kept showing a red (x) around the <?/?> PHP tags part; 
 so, I decided to take those tags out.
  
-I tried typing in to the console: echo "Hello, world!"
+I tried typing into the console: echo "Hello, world!"
 ...but, it wouldn't let me do that, neither; 
 instead, it said press CTRL+C to stop the server running.
 
 Next, I tried going to my own web browser window; and, typing in URL address: 
 0.0.0.0:8080
 ...and, that returned nothing (I figure, because the code is not running on my own home based computer);
-so, I tried typing in both of the following URL's: 
+so, I tried typing in both of the following URL's...(each of which I saw when running the server):   
 
 - https://44.195.90.41/
 - https://44.195.90.41/hw.php  

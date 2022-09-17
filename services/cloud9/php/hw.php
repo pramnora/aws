4d2e462b7ef28cffd29230ebccb551467b072ebd
Paul Ramnora <pramnora@yahoo.com>
2022-09-17 19:15:46 +0100

    Create hw.php
    Initial commit: PHP hello, world!

---
+++ services/cloud9/php/hw.php
@@ -0,0 +1,27 @@
+/*
+ I tried running this PHP script with tags
+
+<?php
+ echo "Hello, world!";
+?>
+
+But, the cloud9 editor kept showing a red (x) around the <?/?> PHP tags part; 
+so, I decided to take those tags out.
+ 
+I tried typing in to the console: echo "Hello, world!"
+...but, it wouldn't let me do that, neither; 
+instead, it said press CTRL+C to stop the server running.
+
+Next, I tried going to my own web browser window; and, typing in URL address: 
+0.0.0.0:8080
+...and, that returned nothing (I figure, because the code is not running on my own home based computer);
+so, I tried typing in...  
+https://44.195.90.41/hw.php  
+...and, that didnt work, neither...?  
+So, eventually, I gave up.../or, at least, temporarily; will return back to try, again...possibly, after reading more.  
+
+ Created: Sat 17 Sep 19:09 PM GMT  
+ Updated: Sat 17 Sep 19:09 PM GMT
+*/
+
+print "Hello, world!";

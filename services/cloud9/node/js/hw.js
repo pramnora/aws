1a9e84157e2a72199cf41536ddfe5cbe6283a1fc
Paul Ramnora <pramnora@yahoo.com>
2022-09-17 21:26:46 +0100

    Create hw.js
    Initial commit...wrote/ran Node .js program...it worked.

---
+++ services/cloud9/node/js/hw.js
@@ -0,0 +1,10 @@
+/* 
+ Wrote 'Hello, world' program...
+ Run With...NojeJS
+ Run result: succeeded
+
+ Created: Sat 17 Sep 2022 21:24 PM GMT
+ Updated: Sat 17 Sep 2022 21:24 PM GMT
+*/
+
+console.log("Hi, from NodeJS!");
